baca6dfac92753d37131d4faaf0c5bdf892fd3db
dksdeepu <115415517+dksdeepu@users.noreply.github.com>
2022-10-09 21:07:01 +0530

    Hacktoberfest 2022

---
+++ 18.py
@@ -0,0 +1,14 @@
+# python code to Check if element exists in list or not
+
+lst = [1, 6, 3, 5, 3, 4]
+#checking if element 7 is present
+# in the given list or not
+i = 7
+# if element present then return
+# exist otherwise not exist
+if i in lst:
+	print("exist")
+else:
+	print("not exist")
+
+	# this code is contributed by gangarajula laxmi
