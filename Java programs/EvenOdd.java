6ddfdc86772ddd0e0879307935b8f248c1a110d0
Sahil <archergaming2601@gmail.com>
2022-10-10 18:20:22 +0530

    Added some Java programs

---
+++ Java programs/EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+    public static void main(String []arg){
+for(int i = 0; i <= 20; i++){
+    if(i%2==0){                               
+        System.out.println(i +" - even");
+    } else {
+        System.out.println(i +" - odd");
+    }
+}
+    }
+    
+}
