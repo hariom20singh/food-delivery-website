930ba29ff599ca54f2209e109086569728edd203
udham31 <116259376+udham31@users.noreply.github.com>
2022-10-20 13:32:15 +0530

    hacktoberfest2022

---
+++ code if else switch .cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main (){
+    //cout<<"this is tutorial 9";
+    int age;
+    cout<<"tell me your age"<<endl;
+    cin>>age;
+    if(age<18){
+        cout<<"you cannot come to party"<<endl;
+
+    }
+    else if(age==18){
+        cout<<"you can cme to the party"<<endl;
+
+    }
+    else{
+        cout<<"you are the  come to the party"<<endl;
+    }
+      return 0;
+
+}
