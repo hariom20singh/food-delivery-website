a4b6b26f17298121cd08bfc9038bd970d4ab055e
Abhiraj Singh Bhadoria <115582321+Abhirajxo@users.noreply.github.com>
2022-10-27 22:01:58 +0530

    HACKTOBERFEST 2022

---
+++ armstrong.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<math.h>
+using namespace std; 
+
+int main(){
+    
+    int n;
+    cin>>n;
+
+    int sum=0;
+    int originaln=n;
+
+    while(n>0){
+        int lastdigit = n%10;
+        sum+=  pow(lastdigit,3);
+        n=n/10;
+    }
+    if(sum==originaln){
+       cout<<"armstrong";
+    }
+    else{
+        cout<<"not armstrong";
+    }
+    cout<<endl;
+    return 0;
+}
